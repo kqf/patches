d7c780d4efa504d8c6b597e983b2bc4da6af95fa
kqf <petrov.knu@gmail.com>
2025-09-04 08:46:00 +0200

    Add the dummy tests (#23)
    * Add the dataset
    
    * Add the plotting
    
    * Fix the parse script location
    
    * Fix the requirements
    
    * Add the agumentations
    
    * Add the pre-commit hooks
    
    * Fix hte formatting
    
    * Fix the typing
    
    * Add the augmentation tests
    
    * Add the training pipeline
    
    * Add the workflows
    
    * Fix hte workflows
    
    * Fix the validation augs
    
    * Fix merge conflicts
    
    * Remove the old workflows
    
    * Fix the dataset validation
    
    * Add the training script
    
    * Fix the tests

---
+++ tests/test_dataset.py
@@ -0,0 +1,20 @@
+import cv2
+
+from patches.dataset import read_dataset
+from patches.plot import plot
+
+
+def dataset():
+    return read_dataset(
+        ".datasets/ground/VisDrone/VisDrone2019-DET-train/annotations.json"
+    )
+
+
+def test_dataset(dataset):
+    for sample in dataset:
+        image = cv2.imread(sample.file_name)
+        if image is None:
+            continue
+        cv2.imshow("frame", plot(image, sample.annotations))
+        # cv2.waitKey()
+    print(len(dataset))
