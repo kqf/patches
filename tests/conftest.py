c54ee0384025167a6bffdccbe4211905b18ed535
kqf <petrov.knu@gmail.com>
2025-09-05 06:59:55 +0200

    Add the proper conftests (#24)
    * Add the dataset
    
    * Add the plotting
    
    * Fix the parse script location
    
    * Fix the requirements
    
    * Add the agumentations
    
    * Add the pre-commit hooks
    
    * Fix hte formatting
    
    * Fix the typing
    
    * Add the augmentation tests
    
    * Add the training pipeline
    
    * Add the workflows
    
    * Fix hte workflows
    
    * Fix the validation augs
    
    * Fix merge conflicts
    
    * Remove the old workflows
    
    * Fix the dataset validation
    
    * Add the training script
    
    * Fix the tests
    
    * Add the proper conftests

---
+++ tests/conftest.py
@@ -0,0 +1,66 @@
+import json
+import pathlib
+
+import cv2
+import numpy as np
+import pytest
+
+
+@pytest.fixture
+def annotations(
+    tmp_path: pathlib.Path,
+    resolution: tuple[int, int] = (640, 480),
+) -> pathlib.Path:
+    image = np.random.randint(0, 256, (*resolution, 3), dtype=np.uint8)
+    image_path = str(tmp_path / "image.png")
+    cv2.imwrite(image_path, image)
+    example = [
+        {
+            "file_name": image_path,
+            "annotations": [
+                {
+                    "bbox": [
+                        0.7375,
+                        0.8722,
+                        0.8145,
+                        0.9333,
+                    ],
+                    "landmarks": [],
+                    "label": "car",
+                    "score": 1.0,
+                    "truncation": 0,
+                    "occlusion": 1,
+                },
+                {
+                    "bbox": [
+                        0.665625,
+                        0.787037,
+                        0.729166,
+                        0.872222,
+                    ],
+                    "landmarks": [],
+                    "label": "person",
+                    "score": 1.0,
+                    "truncation": 0,
+                    "occlusion": 0,
+                },
+                {
+                    "bbox": [
+                        0.61875,
+                        0.73888,
+                        0.68541,
+                        0.83333,
+                    ],
+                    "landmarks": [],
+                    "label": "car",
+                    "score": 1.0,
+                    "truncation": 0,
+                    "occlusion": 0,
+                },
+            ],
+        }
+    ]
+    ofile = tmp_path / "annotations.json"
+    with open(ofile, "w") as f:
+        json.dump(example, f, indent=4)
+    return ofile
