28d71f18bbc2136824f6f74b27d16038dbd4ac0b
kqf <petrov.knu@gmail.com>
2025-09-17 07:41:05 +0200

    Use train test split as test (#44)
    * Add the dataset
    
    * Add the plotting
    
    * Fix the parse script location
    
    * Fix the requirements
    
    * Add the agumentations
    
    * Add the pre-commit hooks
    
    * Fix hte formatting
    
    * Fix the typing
    
    * Add the augmentation tests
    
    * Add the training pipeline
    
    * Add the workflows
    
    * Fix hte workflows
    
    * Fix the validation augs
    
    * Fix merge conflicts
    
    * Remove the old workflows
    
    * Fix the dataset validation
    
    * Add the training script
    
    * Fix the tests
    
    * Add the proper conftests
    
    * Add the tests
    
    * Add the patches to tests
    
    * Update the tests
    
    * Run the pre-commits afterwards
    
    * Fix the .gitignore
    
    * Update the requirements
    
    * Remove numpy limitation
    
    * Unlimit numpy
    
    * BUmp tests
    
    * Drop the tests
    
    * Drop the numpy
    
    * Drop the lightning
    
    * Drop the albumentations
    
    * Remove the torchvision
    
    * Remove torch
    
    * Drop torch completely
    
    * Add the requirements
    
    * Add the torch back
    
    * Fix the requirements.txt
    
    * Fix the requirements
    
    * BUmp the reuquirements
    
    * Stricter requirements
    
    * Fix the dataset
    
    * Restore the original env
    
    * Add the type annotations
    
    * Fix the dataset tests
    
    * No need to validate
    
    * Use train/test split as test
    
    * Turn that into script

---
+++ tests/test_train_split.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+import pytest
+
+from patches.dataset import (
+    Patch,
+    read_dataset,
+    save_dataset,
+    to_patches_dataset,
+)
+
+
+def train_test_split(patches: list[Patch]) -> tuple[list[Patch], list[Patch]]:
+    train_labels = {
+        "person",
+        "truck",
+    }
+    train = [p for p in patches if p.label in train_labels]
+    valid = [p for p in patches if p.label not in train_labels]
+    return train, valid
+
+
+@pytest.fixture
+def datapath(annotations: Path, use_real: bool) -> Path:
+    if not use_real:
+        return annotations
+
+    return Path(
+        ".datasets/ground/VisDrone/VisDrone2019-DET-train/annotations.json",
+    )
+
+
+@pytest.mark.parametrize("use_real", [False])
+def test_splits(datapath: Path, use_real: bool):
+    patches = to_patches_dataset(read_dataset(datapath))
+    save_dataset(str(datapath.with_stem("clean-patches")), patches)
+    train, valid = train_test_split(patches)
+    save_dataset(str(datapath.with_stem("clean-patches-train")), train)
+    save_dataset(str(datapath.with_stem("clean-patches-valid")), valid)
