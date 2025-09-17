4d816dfdc384bbe59461bfab7112adae1645589c
kqf <petrov.knu@gmail.com>
2025-09-17 07:40:43 +0200

    Turn that into script

--- tests/test_train_split.py
+++ tests/test_train_split.py
@@ -21,7 +21,7 @@ def train_test_split(patches: list[Patch]) -> tuple[list[Patch], list[Patch]]:
 
 
 @pytest.fixture
-def dataset(annotations: Path, use_real: bool) -> Path:
+def datapath(annotations: Path, use_real: bool) -> Path:
     if not use_real:
         return annotations
 
@@ -31,9 +31,9 @@ def dataset(annotations: Path, use_real: bool) -> Path:
 
 
 @pytest.mark.parametrize("use_real", [False])
-def test_splits(path: Path):
-    patches = to_patches_dataset(read_dataset(path))
-    save_dataset(str(path.with_stem("clean-patches")), patches)
+def test_splits(datapath: Path, use_real: bool):
+    patches = to_patches_dataset(read_dataset(datapath))
+    save_dataset(str(datapath.with_stem("clean-patches")), patches)
     train, valid = train_test_split(patches)
-    save_dataset(str(path.with_stem("clean-patches-train")), train)
-    save_dataset(str(path.with_stem("clean-patches-valid")), valid)
+    save_dataset(str(datapath.with_stem("clean-patches-train")), train)
+    save_dataset(str(datapath.with_stem("clean-patches-valid")), valid)
