698d77973dd469285c8db549ae7fa8a6bf35f737
kqf <petrov.knu@gmail.com>
2025-10-03 08:56:15 +0100

    Add the missing tests (#76)
    * Make sure the inference works
    
    * Fix the code
    
    * Parametrize the main
    
    * Add the inferencing

---
+++ tests/test_inferences.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import cv2
+import numpy as np
+import pytest
+
+from patches.inference import main
+
+
+@pytest.fixture
+def frame(use_real: bool, tmp_path: Path) -> str:
+    # sourcery skip: no-conditionals-in-tests
+    if use_real:
+        return "image.jpeg"
+
+    # Synthetic 640x480 image
+    frame = np.random.randint(0, 256, (480, 640, 3), dtype=np.uint8)
+    output = str(tmp_path / "testimage.jpg")
+    cv2.imwrite(output, frame)
+    return output
+
+
+def test_inferences(frame):
+    main()
